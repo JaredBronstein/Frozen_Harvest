e6413dd153009a443d1257224bd3dc93f53316a9
JaredBronstein <43156735+JaredBronstein@users.noreply.github.com>
2019-05-02 17:37:16 -0500

    Finished Game Loop

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : InteractiveObject
+{
+    [Tooltip("Check true if Up is the correct position for the Lever to be in. Check false if it is down")]
+    [SerializeField]
+    private bool isUpCorrect;
+
+    private Animator animator;
+    private bool isUp_UseProperty = true;
+
+    public bool IsLeverCorrect => IsUp == isUpCorrect;
+
+    private bool IsUp
+    {
+        get { return isUp_UseProperty;}
+        set
+        {
+            isUp_UseProperty = value;
+            animator.SetBool(nameof(IsUp), value);
+        }
+    }
+
+    public override void InteractWith()
+    {
+        base.InteractWith();
+        IsUp = !IsUp;
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        animator = GetComponent<Animator>();
+    }
+}
--- Assets/Scripts/Puzzle.cs
+++ Assets/Scripts/Puzzle.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Puzzle : MonoBehaviour
+public class Puzzle : InteractiveObject
 {
     [Tooltip("List of Canister Doors for the WAD. Uses Door scripts as the basis so it requires this.")]
     [SerializeField]
     private List<Door> puzzleDoors;
+
+    [Tooltip("List of Levers for the WAD. Uses Lever scripts as the basis so it requires this.")]
+    [SerializeField]
+    private List<Lever> puzzleLevers;
+
+    [Tooltip("The scene the game goes to when it's complete.")]
+    [SerializeField]
+    private string gameSceneName;
 
     /// <summary>
     /// Checks if all the doors are open
@@ -24,5 +33,32 @@ public class Puzzle : MonoBehaviour
             }
         }
         return areAllDoorsOpen;
+    }
+
+    /// <summary>
+    /// Checks if all Levers are in their correct positions
+    /// </summary>
+    /// <returns>Returns true if the levers are all in the correct place, otherwise returns false</returns>
+    private bool CheckIfAllLeversAreCorrect()
+    {
+        bool areAllLeversCorrect = true;
+        foreach (var item in puzzleLevers)
+        {
+            if(!item.IsLeverCorrect)
+            {
+                areAllLeversCorrect = false;
+                break;
+            }
+        }
+        return areAllLeversCorrect;
+    }
+
+    public override void InteractWith()
+    {
+        base.InteractWith();
+        if(CheckIfAllDoorsAreOpen() && CheckIfAllLeversAreCorrect())
+        {
+            SceneManager.LoadScene(gameSceneName);
+        }
     }
 }
